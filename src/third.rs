4fbe0ccf24fdabb9379c3c96b6f9a81a5f0212ae
Stuart Terrett <shterrett@gmail.com>
2016-03-26 14:37:05 -0400

    Persistent Stack iter

--- src/third.rs
+++ src/third.rs
@@ -11,6 +11,10 @@ struct Node<T> {
     next: Link<T>
 }
 
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -33,6 +37,21 @@ impl<T> List<T> {
 
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item>{
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
@@ -62,5 +81,18 @@ mod test {
 
         let list = list.tail();
         assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().append(1).append(2).append(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+
+        assert_eq!(list.head(), Some(&3));
     }
 }
